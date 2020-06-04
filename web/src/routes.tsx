5388f98c02d2c0fa0d6f3cfd87e503db01414459
JWCosta <jonathanwillian80@hotmail.com>
2020-06-04 03:39:04 -0300

    Little changes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import "./App.css";
+import { Route, BrowserRouter } from "react-router-dom";
+
 import Home from "./pages/Home";
 import CreatePoint from "./pages/CreatePoint";
-
-function App() {
-  return <Home />;
-}
-export default App;
